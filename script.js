7324cd7d799f028f9e844e9c0bfbb4ae647f1181
LaurynassGargasas <laurynas.gargasas@gmail.com>
2023-09-08 10:14:11 +0300

    fix-words

--- script.js
+++ script.js
@@ -7,25 +7,25 @@ container.append(container1)
 
 
 let findUs = document.createElement('h1')
-findUs.innerText = 'Find Us'
+findUs.textContent = 'Find Us'
 
 container1.append(findUs)
 
 
 let boxContainer = document.createElement('div')
-boxContainer.className = 'boxContainer'
+boxContainer.classList = 'boxContainer'
 
 container1.append(boxContainer)
 
 
 let box = document.createElement('div')
-box.className = 'box'
+box.classList = 'box'
 
 boxContainer.append(box)
 
 
 let boxTitle = document.createElement('h2')
-boxTitle.innerText = 'Parduotuve 1'
+boxTitle.textContent = 'Parduotuve 1'
 
 box.append(boxTitle)
 
@@ -36,14 +36,14 @@ box.append(paragraph)
 
 
 let phoneTxt = document.createElement('span')
-phoneTxt.innerText = 'Phone :'
+phoneTxt.textContent = 'Phone :'
 
 paragraph.append(phoneTxt)
 
 
 let linkPhone = document.createElement('a')
 linkPhone.setAttribute('href', 'tel: +37061550667')
-linkPhone.innerText = '+37061550667'
+linkPhone.textContent = '+37061550667'
 
 phoneTxt.after(linkPhone)
 
@@ -54,14 +54,14 @@ box.append(paragraph1)
 
 
 let emailTxt = document.createElement('span')
-emailTxt.innerText = 'Email :'
+emailTxt.textContent = 'Email :'
 
 paragraph1.append(emailTxt)
 
 
 let linkEmail = document.createElement('a')
 linkEmail.setAttribute('href', '#')
-linkEmail.innerText = 'shop1@parduotuve.lt'
+linkEmail.textContent = 'shop1@parduotuve.lt'
 
 emailTxt.after(linkEmail)
 
@@ -72,14 +72,14 @@ box.append(paragraph2)
 
 
 let addressTxt = document.createElement('span')
-addressTxt.innerText = 'Address :'
+addressTxt.textContent = 'Address :'
 
 paragraph2.append(addressTxt)
 
 
 let linkAddress = document.createElement('a')
 linkAddress.setAttribute('href', '#')
-linkAddress.innerText = 'shop1@parduotuve.lt'
+linkAddress.textContent = 'shop1@parduotuve.lt'
 
 addressTxt.after(linkAddress)
 
@@ -91,13 +91,13 @@ addressTxt.after(linkAddress)
 
 
 let box2 = document.createElement('div')
-box2.className = 'box'
+box2.classList = 'box'
 
 boxContainer.append(box2)
 
 
 let boxTitle2 = document.createElement('h2')
-boxTitle2.innerText = 'Parduotuve 2'
+boxTitle2.textContent = 'Parduotuve 2'
 
 box2.append(boxTitle2)
 
@@ -108,14 +108,14 @@ box2.append(paragraph31)
 
 
 let phoneTxt2 = document.createElement('span')
-phoneTxt2.innerText = 'Phone :'
+phoneTxt2.textContent = 'Phone :'
 
 paragraph31.append(phoneTxt2)
 
 
 let linkPhone2 = document.createElement('a')
 linkPhone2.setAttribute('href', 'tel: +37061550667')
-linkPhone2.innerText = '+37061550667'
+linkPhone2.textContent = '+37061550667'
 
 phoneTxt2.after(linkPhone2)
 
@@ -126,14 +126,14 @@ box2.append(paragraph22)
 
 
 let emailTxt2 = document.createElement('span')
-emailTxt2.innerText = 'Email :'
+emailTxt2.textContent = 'Email :'
 
 paragraph22.append(emailTxt2)
 
 
 let linkEmail2 = document.createElement('a')
 linkEmail2.setAttribute('href', '#')
-linkEmail2.innerText = 'shop1@parduotuve.lt'
+linkEmail2.textContent = 'shop1@parduotuve.lt'
 
 emailTxt2.after(linkEmail2)
 
@@ -144,14 +144,14 @@ box2.append(paragraph23)
 
 
 let addressTxt2 = document.createElement('span')
-addressTxt2.innerText = 'Address :'
+addressTxt2.textContent = 'Address :'
 
 paragraph23.append(addressTxt2)
 
 
 let linkAddress2 = document.createElement('a')
 linkAddress2.setAttribute('href', '#')
-linkAddress2.innerText = 'shop1@parduotuve.lt'
+linkAddress2.textContent = 'shop1@parduotuve.lt'
 
 addressTxt2.after(linkAddress2)
 
@@ -161,13 +161,13 @@ addressTxt2.after(linkAddress2)
 
 
 let box3 = document.createElement('div')
-box3.className = 'box'
+box3.classList = 'box'
 
 boxContainer.append(box3)
 
 
 let boxTitle3 = document.createElement('h2')
-boxTitle3.innerText = 'Parduotuve 3'
+boxTitle3.textContent = 'Parduotuve 3'
 
 box3.append(boxTitle3)
 
@@ -178,14 +178,14 @@ box3.append(paragraph41)
 
 
 let phoneTxt3 = document.createElement('span')
-phoneTxt3.innerText = 'Phone :'
+phoneTxt3.textContent = 'Phone :'
 
 paragraph41.append(phoneTxt3)
 
 
 let linkPhone3 = document.createElement('a')
 linkPhone3.setAttribute('href', 'tel: +37061550667')
-linkPhone3.innerText = '+37061550667'
+linkPhone3.textContent = '+37061550667'
 
 phoneTxt3.after(linkPhone3)
 
@@ -196,14 +196,14 @@ box3.append(paragraph32)
 
 
 let emailTxt3 = document.createElement('span')
-emailTxt3.innerText = 'Email :'
+emailTxt3.textContent = 'Email :'
 
 paragraph32.append(emailTxt3)
 
 
 let linkEmail3 = document.createElement('a')
 linkEmail3.setAttribute('href', '#')
-linkEmail3.innerText = 'shop1@parduotuve.lt'
+linkEmail3.textContent = 'shop1@parduotuve.lt'
 
 emailTxt3.after(linkEmail3)
 
@@ -214,14 +214,14 @@ box3.append(paragraph33)
 
 
 let addressTxt3 = document.createElement('span')
-addressTxt3.innerText = 'Address :'
+addressTxt3.textContent = 'Address :'
 
 paragraph33.append(addressTxt3)
 
 
 let linkAddress3 = document.createElement('a')
 linkAddress3.setAttribute('href', '#')
-linkAddress3.innerText = 'shop1@parduotuve.lt'
+linkAddress3.textContent = 'shop1@parduotuve.lt'
 
 addressTxt3.after(linkAddress3)
 
@@ -231,13 +231,13 @@ addressTxt3.after(linkAddress3)
 
 
 let box4 = document.createElement('div')
-box4.className = 'box'
+box4.classList = 'box'
 
 boxContainer.append(box4)
 
 
 let boxTitle4 = document.createElement('h2')
-boxTitle4.innerText = 'Parduotuve 4'
+boxTitle4.textContent = 'Parduotuve 4'
 
 box4.append(boxTitle4)
 
@@ -248,14 +248,14 @@ box4.append(paragraph51)
 
 
 let phoneTxt4 = document.createElement('span')
-phoneTxt4.innerText = 'Phone :'
+phoneTxt4.textContent = 'Phone :'
 
 paragraph51.append(phoneTxt4)
 
 
 let linkPhone4 = document.createElement('a')
 linkPhone4.setAttribute('href', 'tel: +37061550667')
-linkPhone4.innerText = '+37061550667'
+linkPhone4.textContent = '+37061550667'
 
 phoneTxt4.after(linkPhone4)
 
@@ -266,14 +266,14 @@ box4.append(paragraph42)
 
 
 let emailTxt4 = document.createElement('span')
-emailTxt4.innerText = 'Email :'
+emailTxt4.textContent = 'Email :'
 
 paragraph42.append(emailTxt4)
 
 
 let linkEmail4 = document.createElement('a')
 linkEmail4.setAttribute('href', '#')
-linkEmail4.innerText = 'shop1@parduotuve.lt'
+linkEmail4.textContent = 'shop1@parduotuve.lt'
 
 emailTxt4.after(linkEmail4)
 
@@ -284,14 +284,14 @@ box4.append(paragraph43)
 
 
 let addressTxt4 = document.createElement('span')
-addressTxt4.innerText = 'Address :'
+addressTxt4.textContent = 'Address :'
 
 paragraph43.append(addressTxt4)
 
 
 let linkAddress4 = document.createElement('a')
 linkAddress4.setAttribute('href', '#')
-linkAddress4.innerText = 'shop1@parduotuve.lt'
+linkAddress4.textContent = 'shop1@parduotuve.lt'
 
 addressTxt4.after(linkAddress4)
 
@@ -301,7 +301,7 @@ addressTxt4.after(linkAddress4)
 
 
 let container2 = document.createElement('div')
-container2.className = 'container2'
+container2.classList = 'container2'
 
 container.append(container2)
 
